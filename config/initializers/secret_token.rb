92ab962fec185c73d2d6f3344d8c772f3101b03f
Jeff Bricco <jbricco@gmail.com>
2014-06-05 13:10:13 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kubbium::Application.config.secret_key_base = '00ee1f4e74df4d9fc74c295bf209eab7a5d5e990a0cb433b7ba2865d43d68ea63c5a8cb3e4667d6aa103e21aa6616e1016e8b10c6388c8b85405a1c42a9b68ac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kubbium::Application.config.session_store :cookie_store, key: '_Kubbium_session'
